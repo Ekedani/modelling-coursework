585baa9d65d9ccbea4a9fba3ae4d5e4e4b23d947
Vladlen <ilusenkovladlen@gmail.com>
2023-12-13 19:37:27 +0200

    finished ConveyorModelFactory

--- src/ConveyorModelFactory.java
+++ src/ConveyorModelFactory.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class ConveyorModelFactory {
     public static Model createBasicConveyorModel() {
+        final int packageCreateDelayMean = 1;
+        final int packageSize = 4;
         final int processorsCount = 5;
         final double processorsDelayMean = 1;
         final int processorsChannelsCount = 1;
@@ -12,19 +14,16 @@ public class ConveyorModelFactory {
         final double transportsDelayMean = 1;
         final double lastTransportDelayMean = 5;
 
-        var packageCreate = new PackageCreate("Package Creator", 1, 4);
-        var processors = createNProcessors(processorsCount, processorsDelayMean, processorsChannelsCount, processorsMaxQueueSize);
-        var transports = createNTransports(processorsCount, transportsDelayMean, lastTransportDelayMean);
-        var dispose = new Dispose("Dispose");
-
-        configureBasicConveyor(packageCreate, processors, transports, dispose);
-
-        var elements = new ArrayList<Element>();
-        elements.add(packageCreate);
-        elements.addAll(processors);
-        elements.addAll(transports);
-        elements.add(dispose);
-        return new Model(elements);
+        return createCustomConveyorModel(
+                packageCreateDelayMean,
+                packageSize,
+                processorsCount,
+                processorsDelayMean,
+                processorsChannelsCount,
+                processorsMaxQueueSize,
+                transportsDelayMean,
+                lastTransportDelayMean
+        );
     }
 
     public static Model createCustomConveyorModel(
@@ -42,7 +41,7 @@ public class ConveyorModelFactory {
         var transports = createNTransports(processorsCount, transportsDelayMean, lastTransportDelayMean);
         var dispose = new Dispose("Dispose");
 
-        configureCustomConveyor(packageCreate, processors, transports, dispose);
+        configureConveyor(packageCreate, processors, transports, dispose);
 
         var elements = new ArrayList<Element>();
         elements.add(packageCreate);
@@ -52,26 +51,33 @@ public class ConveyorModelFactory {
         return new Model(elements);
     }
 
-    private static void configureBasicConveyor(PackageCreate packageCreate, List<ChannelProcess> processors, List<DelayProcess> transports, Dispose dispose) {
-        var conveyorSize = processors.size();
-        var firstProcessor = processors.get(0);
-        packageCreate.addRoutes(
-                new Route(processors.get(0), 1, () -> firstProcessor.getState() == 1),
-                new Route(transports.get(0), 0)
-        );
-        for (int i = 0; i < conveyorSize; i++) {
-            var currentTransport = transports.get(i);
-            var nextProcessor = processors.get((i + 1) % conveyorSize);
-            var nextTransport = transports.get((i + 1) % conveyorSize);
-            currentTransport.addRoutes(
-                    new Route(nextProcessor, 1, () -> nextProcessor.getState() == 1),
-                    new Route(nextTransport, 0)
-            );
+    public static Model createCustomConveyorModel(
+            int packageCreateDelayMean,
+            int packageSize,
+            int processorsCount,
+            double processorsDelayMean,
+            int processorsChannelsCount,
+            int processorsMaxQueueSize,
+            double transportsDelayMean,
+            double lastTransportDelayMean
+    ) {
+        var processorsMaxQueueSizes = new ArrayList<Integer>();
+        for (int i = 0; i < processorsCount; i++) {
+            processorsMaxQueueSizes.add(processorsMaxQueueSize);
         }
-        processors.forEach(processor -> processor.addRoutes(new Route(dispose, 0)));
+        return createCustomConveyorModel(
+                packageCreateDelayMean,
+                packageSize,
+                processorsCount,
+                processorsDelayMean,
+                processorsChannelsCount,
+                processorsMaxQueueSizes,
+                transportsDelayMean,
+                lastTransportDelayMean
+        );
     }
 
-    private static void configureCustomConveyor(
+    private static void configureConveyor(
             PackageCreate packageCreate,
             List<ChannelProcess> processors,
             List<DelayProcess> transports,
@@ -101,17 +107,6 @@ public class ConveyorModelFactory {
 
         }
         processors.forEach(processor -> processor.addRoutes(new Route(dispose, 0)));
-    }
-
-    private static List<ChannelProcess> createNProcessors(int n, double delayMean, int channelsCount, int maxQueueSize) {
-        var processors = new ArrayList<ChannelProcess>();
-        for (int i = 0; i < n; i++) {
-            var name = "Processor " + (i + 1);
-            var processor = new ChannelProcess(name, delayMean, channelsCount, maxQueueSize);
-            processor.setDistribution(Distribution.EXPONENTIAL);
-            processors.add(processor);
-        }
-        return processors;
     }
 
     private static List<ChannelProcess> createNProcessors(int n, double delayMean, int channelsCount, List<Integer> maxQueueSizes) {
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var basicConveyorModel = ConveyorModelFactory.createBasicConveyorModel();
+      /*  var basicConveyorModel = ConveyorModelFactory.createBasicConveyorModel();
         basicConveyorModel.simulate(10000);
-        basicConveyorModel.printResults();
+        basicConveyorModel.printResults();*/
+
+        var customConveyorModel = ConveyorModelFactory.createBasicConveyorModel();
+        customConveyorModel.simulate(1000);
+        customConveyorModel.printResults();
     }
 }
