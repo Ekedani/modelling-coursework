49e3f3d5dc55a7364a3fcaf56cf4bd384624a603
Vladlen <ilusenkovladlen@gmail.com>
2023-12-16 16:07:46 +0200

    Added brute force optimal buffer positions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
         // runBasicModel();
         // observeBasicModelOutputs();
-        runAndCompareBasicAndModifiedModel();
+        // runAndCompareBasicAndModifiedModel();
+        // bruteForceOptimalBufferPositions();
     }
 
     public static void observeBasicModelOutputs() {
@@ -53,7 +59,90 @@ public class Main {
         );
     }
 
-    public static List<Integer> getOptimalBufferPositions() {
-        return null;
+    public static void bruteForceOptimalBufferPositions() {
+        var allResults = getResultsForAllBufferPositions(5, 10);
+        var sortedResults = allResults.entrySet().stream()
+                .sorted((e1, e2) -> {
+                    var e1Value = e1.getValue().get("Mean job time in system (in)");
+                    var e2Value = e2.getValue().get("Mean job time in system (in)");
+                    return Double.compare(e1Value, e2Value);
+                })
+                .toList();
+        for (var entry : sortedResults) {
+            var bufferPositions = entry.getKey();
+            var bufferPositionsString = bufferPositions.stream().map(Object::toString).reduce((s1, s2) -> s1 + ", " + s2).orElse("");
+            var resultsForBufferPositions = entry.getValue();
+            System.out.println("Model with buffer positions: [" + bufferPositionsString + "]");
+            for (var resultEntry : resultsForBufferPositions.entrySet()) {
+                var factor = resultEntry.getKey();
+                var value = resultEntry.getValue();
+                System.out.println(factor + ": " + value);
+            }
+            System.out.println();
+        }
+        System.out.println("Results for each buffer position: ");
+        for (var entry : sortedResults) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().get("Mean job time in system (in)"));
+        }
+        System.out.println("Optimal buffer positions: " + sortedResults.get(0).getKey());
+    }
+
+    public static Map<List<Integer>, Map<String, Double>> getResultsForAllBufferPositions(int processorsCount, int totalBuffersSize) {
+        var allCombinations = generateBufferCombinations(processorsCount, totalBuffersSize);
+        System.out.println("Generated " + allCombinations.size() + " buffer combinations");
+        var allModels = allCombinations.stream()
+                .map(bufferPositions -> ConveyorModelFactory.createCustomConveyorModel(
+                        1,
+                        4,
+                        processorsCount,
+                        1,
+                        1,
+                        bufferPositions,
+                        1,
+                        5
+                ))
+                .toList();
+        var results = new ConcurrentHashMap<List<Integer>, Map<String, Double>>();
+        var executor = Executors.newFixedThreadPool(8);
+        var remainingCounter = new AtomicInteger(allModels.size());
+        for (int i = 0; i < allModels.size(); i++) {
+            var model = allModels.get(i);
+            var bufferPositions = allCombinations.get(i);
+            executor.submit(() -> {
+                var modelResults = ExperimentHelper.doNRuns(model, 2000, 20);
+                var remains = remainingCounter.decrementAndGet();
+                System.out.println(
+                        "Model with buffer positions " + bufferPositions + " finished, " + remains + " models remaining"
+                );
+                results.put(bufferPositions, modelResults);
+            });
+        }
+        executor.close();
+        return results;
+    }
+
+    private static List<List<Integer>> generateBufferCombinations(int processorsCount, int totalBuffersSize) {
+        List<List<Integer>> allCombinations = new ArrayList<>();
+        generateBufferCombinations(processorsCount, totalBuffersSize, new ArrayList<>(), allCombinations);
+        return allCombinations;
+    }
+
+    private static void generateBufferCombinations(
+            int processorsCount,
+            int remainingBufferSize,
+            List<Integer> currentCombination,
+            List<List<Integer>> allCombinations
+    ) {
+        if (processorsCount == 0) {
+            if (remainingBufferSize == 0) {
+                allCombinations.add(new ArrayList<>(currentCombination));
+            }
+            return;
+        }
+        for (int i = 0; i <= remainingBufferSize; i++) {
+            currentCombination.add(i);
+            generateBufferCombinations(processorsCount - 1, remainingBufferSize - i, currentCombination, allCombinations);
+            currentCombination.remove(currentCombination.size() - 1);
+        }
     }
 }
